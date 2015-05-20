420136995889bbd73318af9c469baa9cad3a7221
hamsterofdark <mlandryviolin@gmail.com>
2015-05-19 21:57:46 -0400

    actually added the implementation,

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,11 @@
-print 'hello world'
-
+for i in xrange(1, 101):
+    if i % 15 == 0:
+        print "FizzBuzz"
+    elif i % 3 == 0:
+        print "Fizz"
+    elif i % 5 == 0:
+        print "Buzz"
+    else:
+        print i
+        
 raw_input()
